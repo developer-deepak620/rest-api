903be922b912e5fc43440024622d0bdc706a0ba2
Deepak Kumar <kumardeepak64461@gmail.com>
2025-11-18 14:15:54 +0530

    user

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-const {MongoClient} = require("mongodb");
+const {MongoClient,ObjectId} = require("mongodb");
 const client = new MongoClient("mongodb+srv://deepakkumar456992:123%40deepak@cluster0.gexspmq.mongodb.net/");
 const app = express();
 app.use(cors());
@@ -66,6 +66,73 @@ app.get("/products/:id",async(req,res)=>{
     let resp = await coll.find({id:id}).toArray();
 
     res.json(resp[0]);
+});
+
+
+
+
+
+// customer
+
+app.get("/user",async(req,res)=>{
+
+    await client.connect();
+    let db = client.db("rest_api");
+    let coll = db.collection("user");
+    let resp = await coll.find().toArray();
+
+    res.json(resp);
+});
+
+app.post("/user",async(req,res)=>{
+
+    let data = req.body;
+
+    await client.connect();
+    let db = client.db("rest_api");
+    let user = db.collection("user");
+    let resp = await user.insertOne(data);
+
+    res.json({msg:"Success!! New Record Inserted!!",data:resp});
+                                                                                
+});
+
+app.delete("/user/:id",async(req,res)=>{
+
+    let id = req.params.id;
+    
+    await client.connect();
+    let db = client.db("rest_api");
+    let user = db.collection("user");
+    let resp = await user.deleteOne({_id:ObjectId.createFromHexString(id)});
+
+    res.json({msg:"Succcessfully!! record deeted.",data:resp});
+});
+
+app.put("/user/:id",async(req,res)=>{
+
+    let id = req.params.id;
+    let data = req.body;
+
+    await client.connect();
+    let db = client.db("rest_api");
+    let user = db.collection("user");
+    let resp = await user.updateOne({_id:ObjectId.createFromHexString(id)},{$set:data});
+
+    res.json({msg:"Success!! new customer updated.",data:resp});
+});
+
+app.patch("/user/:id",async(req,res)=>{
+
+    let id = req.params.id;
+    let data = req.body;
+
+    await client.connect();
+    let db = client.db("rest_api");
+    let user = db.collection("user");
+    let resp = await user.updateOne({_id:ObjectId.createFromHexString(id)},{$set:data});
+
+    res.json({msg:"Success!! customer'record updated.",data:resp});
 });
 
 app.listen(8000,()=>console.log("server started on port 8000"));
